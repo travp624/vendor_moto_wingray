4da79e515aa2ec14371340d43f094f044c3ce5a4
travp624 <stygianstudiosdevs@gmail.com>
2012-05-26 01:48:57 -0500

    get things back to the way they should be.

--- proprietary/Android.mk
+++ proprietary/Android.mk
@@ -19,6 +19,18 @@ LOCAL_PATH:=$(call my-dir)
 
 # Module makefile rules for apks on wingray
 
+# MotoLocationProxy
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := MotoLocationProxy
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
+
 # StingrayProgramMenu
 
 include $(CLEAR_VARS)
